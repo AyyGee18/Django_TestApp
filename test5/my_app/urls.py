7dfcb63b051798dfd2099a8970411006f865bff5
Akshin Guseinov <bboysound@yandex.ru>
2023-04-06 00:17:26 +0400

    all files added

---
+++ test5/my_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from my_app import views
+
+# TEMPLATE URLS!
+app_name = "my_app"
+
+urlpatterns = [
+    path('register/', views.register, name="register"),
+    path('login/', views.user_login, name="user_login")
+]
